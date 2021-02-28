cbb8f34eb63ff5a0186f0d8d5cfa4f494186b451
Gabriel Gualteros <Roblox4@gmail.com>
2021-02-28 10:45:05 -0500

    fifth commit added notes to savings and started merit bank

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -3,7 +3,20 @@ package com.meritamerica.assignment1;
 public class MeritAmericaBankApp {
 	public static void main(String[] args) {
 		AccountHolder accountHolder = new AccountHolder("Gabriel", "Fran" , "Gual" , "123456789" , 100.0, 1000.0);
+		System.out.println(accountHolder.toString());
 		
 		
 	}
 }
+//MeritAmericaBankApp
+//public static void main(String[] args)
+//Instantiate an account holder with a checkings balance of $100 and a savings balance of $1000
+//Display the account holder’s toString() output
+//Deposit $500 into the checking account
+//Withdraw $800 from the savings account
+//Display the checking account toString() output
+//Display the savings account toString() output
+//Instantiate another account holder with a checkings balance of $200 and a savings balance of $500
+//Deposit -$500 into the checking account
+//Withdraw $600 from the savings account
+//Display the second account holder’s toString() output
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -5,42 +5,46 @@ public class SavingsAccount {
 	private final double interestRate = 0.0001;
 	private double futureValue = 0;
 	private int years = 0;
-	
-	
+
 	public SavingsAccount(double savingsAccountopeningBalance) {
-		this.balance = savingsAccountopeningBalance; 
+		this.balance = savingsAccountopeningBalance;
 	}
-	
+
 	public double getBalance() {
 		return balance;
 	}
-	
+
 	public double getInterestRate() {
 		return interestRate;
 	}
-	/*this is to withdraw an amount greater than 0 but with the available funds if not its returns a false  */
+
+	/*
+	 * this is to withdraw an amount greater than 0 but with the available funds if
+	 * not its returns a false
+	 */
 	public boolean withdraw(double amount) {
-		if(amount > 0 && amount <= balance) {
+		if (amount > 0 && amount <= balance) {
 			balance -= amount;
 			System.out.print("Your current blance is now :$");
 			return true;
-		}else {
+		} else {
 			System.out.println("You can not withdraw more than your current balance :$");
 			return false;
 		}
 	}
-	/* deposit amount, it has to be an amount thats greater than 0 of course*/
+
+	/* deposit amount, it has to be an amount thats greater than 0 of course */
 	public boolean deposit(double amount) {
-		if(amount > 0) {
+		if (amount > 0) {
 			balance += amount;
 			System.out.println("You current balance is now :$");
 			return true;
-		}else {
+		} else {
 			System.out.println("You can not deposit an amount less then zero!");
 			return false;
 		}
 	}
-	
+
 	public double futureValue(int years) {
 		double value = 0.00;
 		this.years = years;
@@ -49,14 +53,23 @@ public class SavingsAccount {
 		futureValue = value;
 		return futureValue;
 	}
-	
+
 	public String toString() {
 		double futureValue = futureValue(years);
-		return "Savings Account Balance: "+balance+
-				"\n Savings Account Interest Rate" +interestRate+
-				"\n Savings Account Balance in years: "+years+"years: "+futureValue;
+		return "Savings Account Balance: " + balance + "\n Savings Account Interest Rate" + interestRate
+				+ "\n Savings Account Balance in years: " + years + "years: " + futureValue;
 	}
-	
-	
-	
+
 }
+//SavingsAccount
+//SavingsAccount(double openingBalance)
+//double getBalance()
+//double getInterestRate()
+//boolean withdraw(double amount)
+//boolean deposit(double amount)
+//double futureValue(int years)
+//String toString()
+//Sample output:
+//Savings Account Balance: $1000
+//Savings Account Interest Rate: 0.01
+//Savings Account Balance in 3 years: $1030.03
